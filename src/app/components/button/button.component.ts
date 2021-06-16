21f6b652a2e750d48a4194405a488a6c898340fc
Nanlung <nanlung.temlong@gmail.com>
2021-06-16 09:41:30 +0100

    header and button

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-button',
@@ -7,13 +7,17 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ButtonComponent implements OnInit {
   @Input()
-  text!: string;
+  text: string | undefined;
   @Input()
-  color!: string;
+  color: string | undefined;
+  @Output() btnClick = new EventEmitter()
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void { }
+
+  onClick() {
+    this.btnClick.emit();
   }
 
 }
