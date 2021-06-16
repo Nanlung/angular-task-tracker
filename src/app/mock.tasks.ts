df0d4c799f9dbf55342f6f925020052ade902343
Nanlung <nanlung.temlong@gmail.com>
2021-06-16 09:52:46 +0100

    mock data

---
+++ src/app/mock.tasks.ts
@@ -0,0 +1,20 @@
+export const TASKS = [
+  {
+    id: 1,
+    text: 'Doctors Appointment',
+    day: 'June 16th at 12:20pm',
+    reminder: true,
+  },
+  {
+    id: 2,
+    text: 'Meeting at School',
+    day: 'June 18th at 9:00am',
+    reminder: true,
+  },
+  {
+    id: 3,
+    text: 'Food Shopping',
+    day: 'June 25th at 8:00am',
+    reminder: false,
+  }
+]
